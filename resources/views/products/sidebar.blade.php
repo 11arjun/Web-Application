c629feaa33fe6b233a734ab712cd4351ce6f3130
Arjun Shrestha <arjunshrestha@Arjuns-MacBook-Pro.local>
2022-01-26 13:49:18 +0545

    Simple projects showing the use of authentication authorization and  displaying data that allows admin to edit delete update and many more

---
+++ resources/views/products/sidebar.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+
+<div class="sidebar">
+  <a class="active" href="/home">Home</a>
+  <a href="{route{about}}">About</a>
+</div>
+</body>
+</html>
